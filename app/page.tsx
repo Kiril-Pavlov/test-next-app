3dd60c07ea45148a061c8fe40ada5e310d48b761
Kiril-Pavlov <Kire_P@yahoo.com>
2023-11-18 22:47:50 +0100

    Get data from contentful

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,28 @@
 import Image from 'next/image'
+import contentful from '@/app/lib/contentful';
 
-export default function Home() {
+import { createClient } from 'contentful';
+
+const client = createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: process.env.CONTENTFUL_SPACE_ID!,
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
+});
+
+export const getStaticProps = async () => {
+  const res = await client.getEntries({ content_type: 'recipe' })
+
+  return {
+    props: {
+      recipes: res.items,
+      revalidate: 70,
+    },
+  }
+}
+
+export default function Home({recipes}:any) {
+  console.log(recipes)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>Hello World with Next.js and Contentful!!!</h1>
